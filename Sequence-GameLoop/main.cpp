cd401015c43292cea5514597aac8241633afa15f
Charlie Campbell <chacha9669@hotmail.com>
2025-11-03 12:27:09 +1100

    started making player draw

--- Sequence-GameLoop/main.cpp
+++ Sequence-GameLoop/main.cpp
@@ -1,16 +1,45 @@
+#include <chrono>
 #include <raylib.h>
 
 int main (int argv, char* argc[]){
 
-    InitWindow(800,600, "Raylib Starter");
+    constexpr int screenWidth = 1024;
+    constexpr int screenHeight = 768;
+
+    constexpr float groundHeight = 100;
+
+    constexpr float playerSize = 20;
+    Vector2 playerPosition = { 300, screenHeight - (groundHeight + playerSize) };
+
+    InitWindow(screenWidth, screenHeight, "Raylib Starter");
     SetTargetFPS(60);
 
+    static auto previousTime = std::chrono::high_resolution_clock::now();
+
     while (!WindowShouldClose()) {
+        auto now = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration = now - previousTime;
+        previousTime = now;
+
+        float deltaTime = duration.count();
+
+        //update the player
+        constexpr float walkVelocity = 100.0f;
+        if (IsKeyDown(KEY_LEFT))
+        {
+            playerPosition.x -= walkVelocity * deltaTime;
+        }
+        else if (IsKeyDown(KEY_RIGHT))
+        {
+            playerPosition.x += walkVelocity * deltaTime;
+        }
+
 
         BeginDrawing();
 
-        ClearBackground(BLACK);
-        DrawText("Raylib Starter", 10, 10, 20, WHITE);
+        ClearBackground(WHITE);
+        DrawRectangle(0, screenHeight - groundHeight, 2000, 20, BLACK);
+        DrawCircleV(playerPosition, playerSize, RED);
 
         EndDrawing();
     }
